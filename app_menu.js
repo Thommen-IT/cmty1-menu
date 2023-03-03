2cb278daf689fae3a1b22c14a91d431823ad7957
Nick <nick.thommen@gmail.com>
2023-03-03 21:11:13 +0100

    Updated all menus and split title

--- app_menu.js
+++ app_menu.js
@@ -393,22 +393,30 @@ function set_menu() {
                 url: "https://cmty.one/category/sneakers/",
                 icon: ""
               }]
+            },{
+              label: "GALLERY",
+              url: "",
+              icon: ""
             }];
         addCommand("gonative://sidebar/setItems", {"items":items,"enabled": true, "persist":false});
         break;
       case 'supremecommunity.com':
       case 'www.supremecommunity.com':
         var items = [{
+              label: "HOME",
+              url: "https://www.supremecommunity.com/mobile2",
+              icon: ""
+            },{
               label: "DROPLISTS",
               url: "https://www.supremecommunity.com/season/latest/droplists/",
               icon: ""
             },{
-              label: "OVERVIEWS",
+              label: "ITEMS",
               url: "https://www.supremecommunity.com/season/latest/overview/",
               icon: ""
             },{
-              label: "LOOKBOOKS",
-              url: "https://www.supremecommunity.com/season/latest/overview/",
+              label: "LEFT TO DROP",
+              url: "https://www.supremecommunity.com/season/latest/lefttodrop/",
               icon: ""
             },{
               label: "RESTOCKS",
@@ -430,18 +438,54 @@ function set_menu() {
                 url: "https://www.supremecommunity.com/restocks/jpn/",
                 icon: ""
               }]
+            },{
+              label: "SELLOUT TIMES",
+              isGrouping: true, 
+              subLinks: [{
+                label: "EU",
+                url: "https://www.supremecommunity.com/season/latest/times/eu/",
+                icon: "" 
+              }, {
+                label: "UK",
+                url: "https://www.supremecommunity.com/season/latest/time/uk/",
+                icon: ""
+              },{
+                label: "US",
+                url: "https://www.supremecommunity.com/season/latest/time/us/",
+                icon: ""
+              },{
+                label: "JPN",
+                url: "https://www.supremecommunity.com/season/latest/time/jpn/",
+                icon: ""
+              }]
+            },{
+              label: "LOOKBOOKS",
+              url: "https://www.supremecommunity.com/season/latest/overview/",
+              icon: ""
+            },{
+              label: "HELP",
+              url: "https://www.supremecommunity.com/help/",
+              icon: ""
             }];
           gonative.sidebar.setItems({"items":items,"enabled": true, "persist":false});
         break;
       case 'golfwangcommunity.com':
       case 'www.golfwangcommunity.com':
         var items = [{
-              label: "DROPLISTS",
-              url: "https://www.golfwangcommunity.com/droplists/",
+              label: "HOME",
+              url: "https://www.golfwangcommunity.com/mobile2",
               icon: "" 
             },{
-              label: "OVERVIEWS",
-              url: "https://www.golfwangcommunity.com/collection/all/",
+              label: "DROPLISTS",
+              url: "https://www.golfwangcommunity.com/droplists",
+              icon: "" 
+            },{
+              label: "COLLECTIONS",
+              url: "https://www.golfwangcommunity.com/collection",
+              icon: ""
+            },{
+              label: "ITEMS",
+              url: "https://www.golfwangcommunity.com/collection/all",
               icon: ""
             },{
               label: "LOOKBOOKS",
@@ -451,12 +495,64 @@ function set_menu() {
               label: "RESTOCKS",
               url: "https://www.golfwangcommunity.com/restocks/",
               icon: "" 
+            },{
+              label: "LOOKBOOKS",
+              url: "https://www.golfwangcommunity.com/lookbook/latest",
+              icon: "" 
+            },{
+              label: "HELP",
+              url: "https://www.golfwangcommunity.com/help",
+              icon: "" 
             }];
         addCommand("gonative://sidebar/setItems", {"items":items,"enabled": true, "persist":false});
         break;
       case 'palacecmty.com':
       case 'www.palacecmty.com':
-        var items = [];
+        var items = [{
+              label: "HOME",
+              url: "https://www.palacecmty.com/mobile2",
+              icon: "" 
+            },{
+              label: "DROPLISTS",
+              url: "https://www.palacecmty.com/droplists",
+              icon: "" 
+            },{
+              label: "COLLECTIONS",
+              url: "https://www.palacecmty.com/collections",
+              icon: ""
+            },{
+              label: "ITEMS",
+              url: "https://www.palacecmty.com/collections/all",
+              icon: ""
+            },{
+              label: "RESTOCKS",
+              isGrouping: true, 
+              subLinks: [{
+                label: "EU",
+                url: "https://www.palacecmty.com/restocks/eu",
+                icon: "" 
+              }, {
+                label: "UK",
+                url: "https://www.palacecmty.com/restocks/uk",
+                icon: ""
+              },{
+                label: "US",
+                url: "https://www.palacecmty.com/restocks/us",
+                icon: ""
+              },{
+                label: "JPN",
+                url: "https://www.palacecmty.com/restocks/jp",
+                icon: ""
+              }]
+            },{
+              label: "LOOKBOOKS",
+              url: "https://www.palacecmty.com/lookbooks",
+              icon: "" 
+            },{
+              label: "HELP",
+              url: "https://www.palacecmty.com/help",
+              icon: "" 
+            }];
         addCommand("gonative://sidebar/setItems", {"items":items,"enabled": true, "persist":false});
         break;
       default:
@@ -470,7 +566,7 @@ function set_title(title){
 
 function gonative_library_ready(){
     if (window.location.pathname != "/" && window.location.pathname != "/mobile") { 
-        set_title(document.title);
+        set_title(document.title.split(' -')[0]);
     }
     set_menu();
 }
