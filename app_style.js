f408b2862e69a2f01d6fa38dc49a9cef86d5fff5
Nick <nick.thommen@gmail.com>
2024-03-05 11:22:59 +0100

    Create app_style.js

---
+++ app_style.js
@@ -0,0 +1,6 @@
+if (navigator.userAgent.indexOf('cmtyone') > -1) {
+  document.querySelector('.nav-container').style.display = 'none';
+  document.querySelector('.main-container-title-top').style.display = 'none';
+  document.querySelector('.filter-handler').style.display = 'none';
+  document.querySelector('.footer').style.display = 'none';
+}
